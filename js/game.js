991b11764b74e1fdf0e4ef5d5d2a3f47785d26cd
Albert Rondon <hellomyfrend11@hotmail.com>
2019-03-18 22:10:19 +0000

    alot of work

--- js/game.js
+++ js/game.js
@@ -2,45 +2,105 @@
 
 var game = {
     music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    background_image: "kitchen_background_by_tikamifirebird.jpg",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
-            choices: [
-                {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+            message: "You are in the kitchen",
+            choices: [{
+                    text: "make a sandwhich",
+                    nextLevel: "sandwhich",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
-                },
-            ]
-        },
-
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
-            choices: [
-                {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "heat up some pizza",
+                    nextLevel: "fridge",
                 },
             ]
         },
+        sandwhich: {
+            background_image: "sandwich.jpg",
+            music: "Zelda-Overworld.mp3",
+            message: "That was delicious ",
+            choices: [{
+                    text: "next",
+                    nextLevel: "fooditem",
+                }
 
-        field: {
-            message: "Some adventurer you are...",
-            choices: [
-                {
-                    text: "Start over",
-                    nextLevel: "start",
-                },
             ]
         },
 
-    }
+    fooditem: {
+        background_image: "104161884-empty-open-fridge-with-shelves-white-refrigerator-background.jpg",
+        music: "Zelda-Labrynth.mp3",
+        message: "Drink",
+        choices: [{
+            text: "water",
+            nextLevel: "someotherfood",
+        },
+        {
+            text: "juice",
+            nextLevel: "someotherfood",
+            
+        }
+        
+         ]
+    },
+
+    someotherfood: {
+        background_image: "intro-bg.jpg",
+        music: "Stranger-Things-Theme.mp3",
+        message: "ei",
+        choices: [{
+            text: "hielo",
+            nextLevel: "otherfood",
+        }, ]
+    },
+
+    fridge: {
+        background_image: "29722404-appetizing-background-pepperoni-pizza-closeup-filling-the-frame-.jpg",
+        music: "Final-Fantasy-7-Boss-Battle.mp3",
+        message: "You come across a fire monster or something!",
+        choices: [{
+                text: "grab dominoes",
+                nextLevel: "start",
+            },
+            {
+                text: "pizza hut",
+                nextLevel: "close",
+            },
+        ]
+    },
+
+
+
+
+close:{
+   background_image:"1212567.jpg",
+   music:"98_Lost_Mine.mp3",
+    message:"hello",
+    choices: [{
+        text: "cookie",
+        nextLevel:"start",
+    },
+    {
+        text:"or get 2",
+        NextLevel:"dish",
+      
+    }]
+
+    
+},
+dish:{
+    field: {
+        message: "Some adventurer you are...",
+        choices: [{
+            text: "Start over",
+            nextLevel: "start",
+        }, ]
+    },
+
+
+}
+}
 };
